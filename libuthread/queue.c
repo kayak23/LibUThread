3615b0b131ef75681bc245b4409ba0580f08b6df
kayak23 <106361047+kayak23@users.noreply.github.com>
2022-10-25 20:34:47 -0700

    queue API (partial)

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,28 +4,66 @@
 
 #include "queue.h"
 
+#define RET_SUCCESS 0
+#define RET_FAILURE -1
+
+/* Struct queue is implemented as a singly linked list.
+ * data is the void pointer to the contents of the item
+ * next points to the next item in the queue
+ * head points to the front of the queue
+ * tail points to the tail of the queue
+ */
 struct queue {
-	/* TODO Phase 1 */
+	void *data;
+	queue_t next;
+	queue_t head;
+	queue_t tail;
 };
 
 queue_t queue_create(void)
 {
-	/* TODO Phase 1 */
+	queue_t queue;
+	if((queue = malloc(sizeof(struct queue))) == NULL)
+		return NULL;
+	queue->next = NULL;
+	queue->data = NULL;
+	queue->head = queue;
+	queue->tail = queue;
+	return queue;
 }
 
 int queue_destroy(queue_t queue)
 {
-	/* TODO Phase 1 */
+	/* Either the queue DNE or the queue has data */
+	if(queue == NULL || queue->head->data != NULL)
+		return RET_FAILURE;
+	free(queue);
+	return RET_SUCCESS;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	/* TODO Phase 1 */
+	if(queue == NULL || data == NULL)
+		return RET_FAILURE;
+	if((queue->tail->next = malloc(sizeof(struct queue))) == NULL)
+		return RET_FAILURE;
+	queue->data = data;
+	queue->tail = queue->tail->next;
+	queue->tail->next->data = NULL;
+	queue->tail->next->next = NULL;
+	queue->tail->next->head = queue->head;
+	queue->tail->next->tail = queue->tail->next;
+	return RET_SUCCESS;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
-	/* TODO Phase 1 */
+	if(queue == NULL || data == NULL)
+		return RET_FAILURE;
+	if(queue->head->data == NULL)
+		return RET_FAILURE;
+	*data = queue->head->data;
+	queue->head = queue->next;
 }
 
 int queue_delete(queue_t queue, void *data)
