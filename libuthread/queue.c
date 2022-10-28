411223596c6a1b0c24c0bc87bf3f2e0e047a1294
Raf <rafmillan@ucdavis.edu>
2022-10-27 20:23:49 -0700

    sem done

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -1,100 +1,80 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h> //remove after debug version
-
-#ifndef QUEUE_H
-#define QUEUE_H
 #include "queue.h"
-#endif
 
-#ifndef RET_SUCCESS
 #define RET_SUCCESS 0
-#endif
-#ifndef RET_FAILURE
 #define RET_FAILURE -1
-#endif
 
 typedef struct q_node* node_t;
 
 struct q_node {
-	node_t next;
-	node_t prev;
-	void *data;
+        node_t next;
+        node_t prev; //TODO: eliminate if unused
+        void *data;
 };
 
 struct queue {
-	node_t head;
-	node_t tail;
-	int len;
+        node_t head;
+        node_t tail;
+        int len;
 };
 
 queue_t queue_create(void)
 {
-	queue_t q;
-	if((q = malloc(sizeof(struct queue))) == NULL)
-		return NULL;
-	q->head = NULL;
-	q->tail = NULL;
-	q->len = 0;
-	return q;
+        queue_t q;
+        if((q = malloc(sizeof(struct queue))) == NULL)
+                return NULL;
+        q->head = NULL;
+        q->tail = NULL;
+        q->len = 0;
+        return q;
 }
 
 int queue_destroy(queue_t queue)
 {
-	if (queue == NULL || queue->len != 0) 
-		return RET_FAILURE;
-	free(queue);
+        if (queue == NULL || queue->len != 0) 
+                return RET_FAILURE;
+        free(queue);
         return RET_SUCCESS;
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
-	node_t node;
-	if (queue == NULL || data == NULL) 
-		return RET_FAILURE;
-	if((node = malloc(sizeof(struct q_node))) == NULL)
-		return RET_FAILURE;
-	//fprintf(stderr, "[enqueue] Beginning enqueue procedure\n");
+        node_t node;
+        if (queue == NULL || data == NULL) 
+                return RET_FAILURE;
+        if((node = malloc(sizeof(struct q_node))) == NULL)
+                return RET_FAILURE;
         node->data = data;
         node->next = NULL;
         node->prev = NULL;
-	//fprintf(stderr, "[enqueue] Values init'd\n");
         if (queue->len == 0) {
                 queue->head = node;
                 queue->tail = node;
         } else {
-		/*if(queue->tail == NULL) {
-			fprintf(stderr, "[enqueue] Error! Null tail pointer!\n");
-			return RET_FAILURE;
-		}*/
-		//fprintf(stderr, "[enqueue] Setting queue params...");
                 queue->tail->next = node;
-		//fprintf(stderr, "tail->next done...");
                 node->prev = queue->tail;
-		//fprintf(stderr, "node->prev done...");
                 queue->tail = node;
-		//fprintf(stderr, "queue->tail done\n");
         }
         queue->len++;
-	//fprintf(stderr, "[enqueue] Enqueue procedure completed\n");
         return RET_SUCCESS;
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
         if (queue == NULL || data == NULL || queue_length(queue) == 0) 
-		return RET_FAILURE;
+                return RET_FAILURE;
 
-	*data = queue->head->data;
-	node_t new_head = queue->head->next;
+        *data = queue->head->data;
 
         queue->head->data = NULL;
         queue->head->next = NULL;
         queue->head->prev = NULL;
-	free(queue->head);
+        node_t new_head = queue->head->next;
+        free(queue->head);
         queue->head = new_head;
-	
+        
         /* If queue is empty */
         if (queue->head == NULL)
                 queue->tail = NULL;
@@ -102,49 +82,48 @@ int queue_dequeue(queue_t queue, void **data)
                 queue->head->prev = NULL;
 
         queue->len--;
-	//fprintf(stderr, "[dequeue] Queue size is now %d\n", queue->len);
         return RET_SUCCESS;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
-	if (queue == NULL || data == NULL)
-		return RET_FAILURE;
-	node_t iter = queue->head;
-	while (iter != NULL) {
-		if (iter->data == data) {
-			if (iter != queue->tail)
-				iter->next->prev = iter->prev;
-			if (iter != queue->head)
-				iter->prev->next = iter->next;
-			if (iter == queue->head)
-				queue->head = iter->next;
-			if (iter == queue->tail)
-				queue->tail = iter->prev;
-			iter->next = NULL;
-			iter->prev = NULL;
-			iter->data = NULL;
-			queue->len--;
-			free(iter);
-			return RET_SUCCESS;
-		}
-		iter = iter->next;
-	}
-	return RET_FAILURE;
+        if (queue == NULL || data == NULL)
+                return RET_FAILURE;
+        node_t iter = queue->head;
+        while (iter != NULL) {
+                if (iter->data == data) {
+                        if (iter != queue->tail)
+                                iter->next->prev = iter->prev;
+                        if (iter != queue->head)
+                                iter->prev->next = iter->next;
+                        if (iter == queue->head)
+                                queue->head = iter->next;
+                        if (iter == queue->tail)
+                                queue->tail = iter->prev;
+                        iter->next = NULL;
+                        iter->prev = NULL;
+                        iter->data = NULL;
+                        queue->len--;
+                        free(iter);
+                        return RET_SUCCESS;
+                }
+                iter = iter->next;
+        }
+        return RET_FAILURE;
 }
 
 int queue_iterate(queue_t queue, queue_func_t func)
 {
-	if (queue == NULL || func == NULL)
-		return RET_FAILURE;
-	node_t iter = queue->head;
-	node_t next;
-	while (iter != NULL) {
-		next = iter->next;
-		func(queue, iter->data);
-		iter = next;
-	}
-	return RET_SUCCESS;
+        if (queue == NULL || func == NULL)
+                return RET_FAILURE;
+        node_t iter = queue->head;
+        node_t next;
+        while (iter != NULL) {
+                next = iter->next;
+                func(queue, iter->data);
+                iter = next;
+        }
+        return RET_SUCCESS;
 }
 
 int queue_length(queue_t queue)
@@ -153,17 +132,16 @@ int queue_length(queue_t queue)
 }
 
 #include <stdio.h>
-/*
+
 static void q_print(queue_t queue, void *data)
 {
-	printf("%s\n", (char*)data);
+        printf("%s\n", (char*)data);
 }
 
 static void q_delete(queue_t queue, void *data)
 {
-	queue_delete(queue, data);
+        queue_delete(queue, data);
 }
-
 
 int main() {
         queue_t queue;
@@ -177,25 +155,25 @@ int main() {
         printf("queue head %s\n", (char*) queue->head->data);
         printf("queue tail %s\n", (char*) queue->tail->data);
 
-	queue_delete(queue, "aa");
+        queue_delete(queue, "aa");
 
         printf("queue head %s\n", (char*) queue->head->data);
         printf("queue tail %s\n", (char*) queue->tail->data);
-	queue_enqueue(queue, a);
-	queue_enqueue(queue, a);
-	queue_iterate(queue, q_print);
-	queue_iterate(queue, q_delete);
-	queue_iterate(queue, q_print);
-	queue_enqueue(queue, "HelloWorld");
-	printf("enqueued\n");
-	void **collector = (void**)&a;
-	void **trash = NULL;
-	int retval = queue_dequeue(queue, trash);
-	printf("dequeued\n");
-	if(retval == -1)
-		printf("dequeue failed\n");
-	printf("Collected value: %s\n", *(char**)collector);
-	printf("Done!\n");
+        queue_enqueue(queue, a);
+        queue_enqueue(queue, a);
+        queue_iterate(queue, q_print);
+        queue_iterate(queue, q_delete);
+        queue_iterate(queue, q_print);
+        queue_enqueue(queue, "HelloWorld");
+        printf("enqueued\n");
+        void **collector = (void**)&a;
+        void **trash = NULL;
+        int retval = queue_dequeue(queue, trash);
+        printf("dequeued\n");
+        if(retval == -1)
+                printf("dequeue failed\n");
+        printf("Collected value: %s\n", *(char**)collector);
+        printf("Done!\n");
         return 0;
-}*/
+}
 
--- libuthread/sem.c
+++ libuthread/sem.c
@@ -5,27 +5,90 @@
 #include "sem.h"
 #include "private.h"
 
+#ifndef RET_FAILURE
+#define RET_FAILURE -1
+#endif
+
+#ifndef RET_SUCCESS
+#define RET_SUCCESS 0
+#endif
+
 struct semaphore {
-	/* TODO Phase 3 */
+	int count;
+	queue_t q_blocked;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* TODO Phase 3 */
+	/* Allocate space for semaphore*/
+	sem_t sem = malloc(sizeof(struct semaphore));
+	if (sem == NULL)
+		return NULL;
+	
+	/* Initiate members of sem */
+	sem->count = count;
+	sem->q_blocked = queue_create();
+
+	return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-	/* TODO Phase 3 */
+	if (sem == NULL || queue_length(sem->q_blocked) != 0)
+		return RET_FAILURE;
+
+	/* destroy q_blocked */
+	queue_destroy(sem->q_blocked);
+
+	free(sem);
+	return RET_SUCCESS;
 }
 
 int sem_down(sem_t sem)
-{
-	/* TODO Phase 3 */
+{	
+	/*
+ 	 * spinlock_lock(sem->lock);
+     * while (sem->count == 0) {
+     *     // Block self //
+     * }
+     * sem->count -= 1;
+     * spinlock_unlock(sem->lock);
+	 */
+	
+	if (sem == NULL)
+		return RET_FAILURE;
+
+	/* If sem count is zero, we block ourselves */
+	if (sem->count == 0) {
+		uthead_t self = uthread_current();
+		queue_enqueue(sem->q_blocked, self);
+		uthread_block();
+	}
+
+	sem->count--;
+	return RET_SUCCESS;
 }
 
 int sem_up(sem_t sem)
 {
-	/* TODO Phase 3 */
+	/*
+	 * spinlock_lock(sem->lock);
+     * sem->count += 1;
+     * / Wake up first in line /
+     * / (if any)              /
+     * ...
+     * spinlock_unlock(sem->lock);
+	 */
+	if (sem == NULL)
+		return RET_FAILURE;
+	
+	if (queue_length(sem->q_blocked) != 0) {
+		uthread_t oldest;
+		queue_dequeue(sem->q_blocked, (void**) &oldest);
+		uthread_unblock(oldest);
+	}
+
+	sem->count++;
+	return RET_SUCCESS;
 }
 
