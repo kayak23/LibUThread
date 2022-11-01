ffd0e42922dcb520eac8682c66d20cf16eea01b8
Raf <rafmillan@ucdavis.edu>
2022-11-01 16:25:03 -0700

    Removed print statements, formatted tabs to spaces, this file is complete

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -25,95 +25,68 @@
 
 typedef struct uthread_tcb* uthread_t;
 
+/*
+ * Semaphore data structure
+ * - count: internal semaphore count
+ * - q_blocked: list of blocked threads
+ */
 struct semaphore {
-	int count;
-	queue_t q_blocked;
+        int count;
+        queue_t q_blocked;
 };
 
 sem_t sem_create(size_t count)
 {
-	/* Allocate space for semaphore*/
-	//fprintf(stderr, "[sem_create] Creating a semaphore...");
-	sem_t sem = malloc(sizeof(struct semaphore));
-	if (sem == NULL)
-		return NULL;
-	
-	/* Initiate members of sem */
-	sem->count = count;
-	sem->q_blocked = queue_create();
-	//fprintf(stderr, "...semaphore created successfully.\n");
-	return sem;
+        /* Allocate space for semaphore*/
+        sem_t sem = malloc(sizeof(struct semaphore));
+        if (sem == NULL)
+                return NULL;
+        
+        /* Initiate members of sem */
+        sem->count = count;
+        sem->q_blocked = queue_create();
+        return sem;
 }
 
 int sem_destroy(sem_t sem)
 {
-	//fprintf(stderr, "[sem_destory] Destroying a semaphore...");
-	if (sem == NULL || queue_length(sem->q_blocked) != 0)
-		return RET_FAILURE;
-
-	/* destroy q_blocked */
-	queue_destroy(sem->q_blocked);
-
-	free(sem);
-	//fprintf(stderr, "...semaphore destroyed successfully.\n");
-	return RET_SUCCESS;
+        if (sem == NULL || queue_length(sem->q_blocked) != 0)
+                return RET_FAILURE;
+        queue_destroy(sem->q_blocked);
+        free(sem);
+        return RET_SUCCESS;
 }
 
 int sem_down(sem_t sem)
 {
-	// we should probably make this a critical section.
-	// TODO: implement spinlocks
-	
-	/*
- 	 * spinlock_lock(sem->lock);
-     * while (sem->count == 0) {
-     *     // Block self //
-     * }
-     * sem->count -= 1;
-     * spinlock_unlock(sem->lock);
-	 */
-	//fprintf(stderr, "[sem_down] Down procedure invoked.\n");
-	if (sem == NULL)
-		return RET_FAILURE;
-	/* If sem count is zero, we block ourselves */
-	while (sem->count == 0) {
-		//fprintf(stderr, "[sem_down] out of resources; blocking thread\n");
-		uthread_t self = uthread_current();
-		queue_enqueue(sem->q_blocked, self);
-		uthread_block();
-	}
+        // we should probably make this a critical section.
 
-	sem->count--;
-	//fprintf(stderr, "[sem_down] Down procedure completed.\n");
-	return RET_SUCCESS;
+        if (sem == NULL)
+                return RET_FAILURE;
+
+        /* If sem count is zero, we block ourselves */
+        while (sem->count == 0) {
+                uthread_t self = uthread_current();
+                queue_enqueue(sem->q_blocked, self);
+                uthread_block();
+        }
+        sem->count--;
+        return RET_SUCCESS;
 }
 
 int sem_up(sem_t sem)
 {
-	// we should probably make this a critical sectioni
-	// TODO: implement spinlocks
+        // we should probably make this a critical section
 
-	/*
-	 * spinlock_lock(sem->lock);
-     * sem->count += 1;
-     * / Wake up first in line /
-     * / (if any)              /
-     * ...
-     * spinlock_unlock(sem->lock);
-	 */
-	//fprintf(stderr, "[sem_up] Up procedure invoked.\n");
-	if (sem == NULL)
-		return RET_FAILURE;
-	
-	if (queue_length(sem->q_blocked) != 0) {
-		//fprintf(stderr, "[sem_up] There are blocked threads. Unlocking...");
-		uthread_t oldest;
-		queue_dequeue(sem->q_blocked, (void**) &oldest);
-		uthread_unblock(oldest);
-		//fprintf(stderr, "...unblocking successful.\n");
-	}
-	//fprintf(stderr, "[sem_up] Up procedure completed.\n");
-	sem->count++;
-	return RET_SUCCESS;
+        if (sem == NULL)
+                return RET_FAILURE;
+        
+        /* unblock the oldest thread in q_blocked */
+        if (queue_length(sem->q_blocked) != 0) {
+                uthread_t oldest;
+                queue_dequeue(sem->q_blocked, (void**) &oldest);
+                uthread_unblock(oldest);
+        }
+        sem->count++;
+        return RET_SUCCESS;
 }
-
