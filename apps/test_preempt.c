3c1f19d5f0bb688718fbe098a235f1c3d69ef0bc
kayak23 <106361047+kayak23@users.noreply.github.com>
2022-11-02 22:17:41 -0700

    Update test_preempt.c

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -5,23 +5,24 @@
 #include "uthread.h"
 
 #define LONGEST 36
-
+/*
 static int interrupts;
 static int format;
 struct timeval t1, t2;
 double t_elapsed;
-
+*/
+/*
 static void interrupt(void *arg)
 {
 	(void)arg;
 	interrupts = 1;
-}
+}*/
 
 /* greedy never yields so it will stay in the loop
  * until interrupt is scheduled. This can only happen
  * if the virtual timer forcibly yields greedy.
  * */
-static void greedy(void *arg)
+/*static void greedy(void *arg)
 {
         gettimeofday(&t1, NULL);
 
@@ -31,21 +32,33 @@ static void greedy(void *arg)
 	fprintf(stdout, "*** TEST Preemption ***%n", &format);
 	fflush(stdout);
 	while (interrupts != 0) {
-		if (first == 0) {
-			first = 1;
-			uthread_create(interrupt, NULL);
-		}
+		
 	}
 	for (i = 0; i < LONGEST-format; i++) fprintf(stdout, " ");
-	
-        gettimeofday(&t2, NULL);
-        t_elapsed = (t2.tv_sec - t1.tv_sec) * 1000.0;
-        t_elapsed += (t2.tv_usec - t1.tv_usec) / 1000.0;
-        fprintf(stdout, "\nTime: %f ms ... \033[32m[PASS]\033[0m\n", t_elapsed);
+        fprintf(stdout, " ... \033[32m[PASS]\033[0m\n", t_elapsed);
+}*/
+
+static void loop1(void *data)
+{
+	(void)data;
+	while(true) fprintf(stdout, "loop A\n");
+}
+
+static void loop2(void *data)
+{
+	(void)data;
+	while(true) fprintf(stdout, "loop B\n");
+}
+
+static void driver(void* data)
+{
+	(void)data;
+	uthread_create(loop1, NULL);
+	uthread_create(loop2, NULL);
 }
 
 int main(void)
 {
-	uthread_run(true, greedy, NULL);
+	uthread_run(true, driver, NULL);
 	return EXIT_SUCCESS;
 }
