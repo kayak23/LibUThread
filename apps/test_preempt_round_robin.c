a3e729afc815cb2126c6f130f2751262e8cf704a
kayak23 <106361047+kayak23@users.noreply.github.com>
2022-11-02 23:06:42 -0700

    preempt tests (occasionally hang)

--- apps/test_preempt_round_robin.c
+++ apps/test_preempt_round_robin.c
@@ -8,17 +8,24 @@
 
 static int countA = 0;
 static int countB = 0;
+static int countC = 0;
 
 static void loopA(void *data)
 {
 	(void)data;
-	while(countA + countB < SUM) fprintf(stdout, "countA: %d\n", countA++);
+	while(countA + countB + countC < SUM) fprintf(stdout, "loop A %d\n", countA++);
 }
 
 static void loopB(void *data)
 {
 	(void)data;
-	while(countA + countB < SUM) fprintf(stdout, "countB: %d\n", countB++);
+	while(countA + countB + countC < SUM) fprintf(stdout, "loop B %d\n", countB++);
+}
+
+static void loopC(void *data)
+{
+  	(void)data;
+  	while(countA + countB + countC < SUM) fprintf(stdout, "loop C %d\n", countC++);
 }
 
 static void driver(void* data)
@@ -26,11 +33,12 @@ static void driver(void* data)
 	(void)data;
 	uthread_create(loopA, NULL);
 	uthread_create(loopB, NULL);
+  	uthread_create(loopC, NULL);
 }
 
 int main(void)
 {
 	uthread_run(true, driver, NULL);
-	fprintf(stdout, "countA: %d, countB: %d\n", countA, countB);
+	fprintf(stdout, "countA: %d, countB: %d, countC: %d\n", countA, countB, countC);
 	return EXIT_SUCCESS;
 }
